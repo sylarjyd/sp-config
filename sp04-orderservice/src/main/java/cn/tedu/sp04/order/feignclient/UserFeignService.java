5e5765ee89ad5c59ded1b68b9057dea0486eb00c
whot <whot@qq.com>
2020-01-20 09:21:36 +0800

    springcloud微服务搭建代码

---
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/order/feignclient/UserFeignService.java
@@ -0,0 +1,20 @@
+package cn.tedu.sp04.order.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.tedu.sp01.pojo.User;
+import cn.tedu.web.util.JsonResult;
+
+@FeignClient(name="user-service", fallback = UserFeignServiceFB.class)
+@Component(value = "userService")
+public interface UserFeignService {
+	@GetMapping("/{userId}")
+	JsonResult<User> getUser(@PathVariable Integer userId);
+
+	@GetMapping("/{userId}/score") 
+	JsonResult addScore(@PathVariable Integer userId, @RequestParam Integer score);
+}
